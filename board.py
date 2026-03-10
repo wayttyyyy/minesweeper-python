7c080a6838de45fa87ef7447fb5e92404c17ba51
Голдинський Дмитро <dimagoldinskij@gmail.com>
2026-03-10 02:29:09 +0200

    Implement random mine placement

--- board.py
+++ board.py
@@ -10,3 +10,14 @@ class Board:
         self.grid = [[Cell(r, c) for c in range(cols)] for r in range(rows)]
         self.game_over = False
         self.win = False
+
+        self._place_mines()
+
+    def _place_mines(self):
+        mines_placed = 0
+        while mines_placed < self.mines:
+            r = random.randint(0, self.rows - 1)
+            c = random.randint(0, self.cols - 1)
+            if not self.grid[r][c].is_mine:
+                self.grid[r][c].is_mine = True
+                mines_placed += 1
