648ed26bb8ac884746bda270d9d5990f27571772
Salvi Denis <salvi.denis2007@gmail.com>
2026-03-13 08:32:32 +0200

    Check and save new best time on win

--- gui.py
+++ gui.py
@@ -167,7 +167,11 @@ class MinesweeperGUI:
             for c in range(self.board.cols):
                 if self.board.grid[r][c].is_mine:
                     self.board.grid[r][c].is_revealed = True
-    
+    def check_best_time(self):
+        current_best = self.best_times.get(self.difficulty_key, float('inf'))
+        if self.elapsed_time < current_best:
+            self.best_times[self.difficulty_key] = self.elapsed_time
+            self.save_best_times()
     def update_mines_label(self):
         remaining = self.board.mines - self.board.flags_placed
         self.mines_label.config(text=f"Мін: {remaining}")
