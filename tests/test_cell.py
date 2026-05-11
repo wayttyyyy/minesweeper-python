1a9a0badc74b156e3463a239be6cab9d6fcc80de
Корзун Олександр <olexandr.korzun@gmail.com>
2026-05-12 02:03:06 +0300

    test: add initialization unit test for Cell class

---
+++ tests/test_cell.py
@@ -0,0 +1,12 @@
+from cell import Cell
+
+def test_cell_initialization():
+    """Перевіряє правильність початкового стану клітинки."""
+    cell = Cell(row=2, col=3)
+    
+    assert cell.row == 2
+    assert cell.col == 3
+    assert cell.is_mine is False
+    assert cell.is_revealed is False
+    assert cell.is_flagged is False
+    assert cell.adjacent_mines == 0
